32b969c0906d4ab13bf7790f4ed6688539f7901a
JethroNatividad <natividad.jet@gmail.com>
2024-01-15 15:01:02 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 // program that takes in two strings, and checks if they are anagram or not.
+// Inputs: str1, str2
+// process: check anagram
+// output: "str1" and "str2" {are | are not} anagrams
 
 fn main() {
     println!("Hello, world!");
