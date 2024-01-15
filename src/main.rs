880871213466816c22246c617fddb0d9a1782c7e
JethroNatividad <natividad.jet@gmail.com>
2024-01-16 00:42:05 +0800

    implement get input function

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,23 @@ mod tests {
     }
 }
 
+fn get_input<T: std::str::FromStr>(prompt: &str) -> T {
+    loop {
+        print!("{}", prompt);
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+
+        match input.trim().parse() {
+            Ok(value) => break value,
+            Err(_) => println!("Invalid input. Please try again."),
+        }
+    }
+}
+
 fn main() {
     // print "Enter two strings and I'll tell you if they are anagrams."
     // str1 input "Enter the first string: "
